attention mask fix

bert.py
@@ -183,9 +183,11 @@ class BertModel(object):
             with tf.variable_scope("encoder"):
                 # Run the stacked transformer.
                 # `sequence_output` shape = [batch_size, seq_length, hidden_size].
+                attention_mask = transformer.create_attention_mask_from_input_mask(
+                    input_ids, input_mask)
                 self.sequence_output = transformer.transformer_encoder(
                     input_tensor=self.embedding_output,
-                    attention_mask=input_mask,
+                    attention_mask=attention_mask,
                     hidden_size=config.hidden_size,
                     num_encoder_layers=config.num_hidden_layers,
                     num_attention_heads=config.num_attention_heads,

run_bert_squad.py
@@ -317,7 +317,7 @@ def input_fn_builder(input_file, seq_length, is_training, drop_remainder):
             data = data.shuffle(buffer_size=100)
 
         data = data.apply(
-            tf.contrib.data.map_and_batch(
+            tf.data.experimental.map_and_batch(
                 lambda record: _decode_record(record, name_to_features),
                 batch_size=batch_size,
                 drop_remainder=drop_remainder))

transformer.py
@@ -42,6 +42,7 @@ def multiheaded_attention(
 
     Args:
     query, key, value: the respective attention vectors.
+    mask: of shape [batch_size, seq_length, seq_length].
     embedding_size: the dimension of the input tensor embedding
     hidden_size: the size of the model after projection for attention
     head_count: the number of parallel attention count.
@@ -145,7 +146,7 @@ def transformer_encoder(input_tensor,
 
     Args:
         input_tensor: of shape (batch_size, seq_length, embedding_size)
-        attention_mask: of shape (batch_size, seq_length)
+        attention_mask: of shape (batch_size, seq_length, seq_length)
         hidden_size: the dimension in multiheaded attention after projection
         num_encoder_layers: number of transformer encoder layers
         num_attention_heads: number of attention heads
@@ -209,3 +210,36 @@ def transformer_encoder(input_tensor,
                     inputs=prev_output, begin_norm_axis=-1, begin_params_axis=-1)
 
     return prev_output
+
+def create_attention_mask_from_input_mask(from_tensor, to_mask):
+    """Create 3D attention mask from a 2D tensor mask.
+
+    Args:
+        from_tensor: 2D or 3D Tensor of shape [batch_size, from_seq_length, ...].
+        to_mask: int32 Tensor of shape [batch_size, to_seq_length].
+
+    Returns:
+        float Tensor of shape [batch_size, from_seq_length, to_seq_length].
+    """
+    from_shape = from_tensor.shape.as_list()
+    batch_size = from_shape[0]
+    from_seq_length = from_shape[1]
+
+    to_shape = to_mask.shape.as_list()
+    to_seq_length = to_shape[1]
+
+    to_mask = tf.cast(
+        tf.reshape(to_mask, [batch_size, 1, to_seq_length]), tf.float32)
+
+    # We don't assume that `from_tensor` is a mask (although it could be). We
+    # don't actually care if we attend *from* padding tokens (only *to* padding)
+    # tokens so we create a tensor of all ones.
+    #
+    # `broadcast_ones` = [batch_size, from_seq_length, 1]
+    broadcast_ones = tf.ones(
+        shape=[batch_size, from_seq_length, 1], dtype=tf.float32)
+
+    # Here we broadcast along two dimensions to create the mask.
+    mask = broadcast_ones * to_mask
+
+    return mask