fix the attention mask bug

model_decoder.py
@@ -45,8 +45,10 @@ def squad_v2_decoder(
         expanded_weights = tf.broadcast_to(
             output_weights,
             (tf.shape(sequence_output)[0], 1, embedding_size))
+        attention_mask = transformer.create_attention_mask_from_input_mask(
+            sequence_output, input_mask)
         pooled_output, _ = transformer.multiheaded_attention(
-            expanded_weights, sequence_output, sequence_output, input_mask,
+            expanded_weights, sequence_output, sequence_output, attention_mask,
             hidden_size=1024,
             head_count=16,
             dropout_prob=dropout_prob,

transformer.py
@@ -107,7 +107,7 @@ def multiheaded_attention(
         mask = tf.expand_dims(mask, axis=[1])
         # The original masks have 1 for valid positions and 0 for
         # invalid positions, the transformed adder would have
-        # value 0 for valie position and -10000 for invalid positions.
+        # value 0 for valid position and -10000 for invalid positions.
         adder = (1.0 - tf.cast(mask, tf.float32)) * (-10000.0)
         attention_scores += adder
 